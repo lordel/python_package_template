fe20b18d2c059f681c6a81c5381b2e7e874b32ac
Lorenzo Delcroix <lordel@users.noreply.github.com>
2024-02-09 14:58:48 -0600

    Delete setup.py
    Not needed with Hatch and pyproject.toml

--- setup.py
+++
@@ -1,8 +0,0 @@
-"""Setup file for the repository. This is a basic file for a python package.
-
-Even for small projects, you should include these because of editable installs.
-The proper configuration for the package is specified in the setup.cfg file.
-"""
-from setuptools import setup
-
-setup()
